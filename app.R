741de5506436dcc07bc4034cb90fe54d824be5db
Aditya Prakash <147525454+adityaprakash171328@users.noreply.github.com>
2025-07-06 18:01:27 +0530

    Add files via upload

---
+++ app.R
@@ -0,0 +1,50 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+
+
+ui <- fluidPage(
+
+   
+    titlePanel("Old Faithful Geyser Data"),
+
+     
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+      
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+       
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             xlab = 'Waiting time to next eruption (in mins)',
+             main = 'Histogram of waiting times')
+    })
+}
+
+shinyApp(ui = ui, server = server)
